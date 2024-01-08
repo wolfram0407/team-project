16bb30c9694f76911713d89260341b217d84a812
zmjjkk98 <ysyt3199@gmail.com>
2024-01-08 12:09:45 +0900

    add: 진짜  카드 생성

---
+++ src/card/card.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCardDto } from './dto/create-card.dto';
+import { UpdateCardDto } from './dto/update-card.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Card } from './entities/card.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CardService {
+  constructor(@InjectRepository(Card) private readonly cardRepository:Repository<Card>){
+  }
+
+  async create(createDto: CreateCardDto) {
+    await this.cardRepository.save(createDto)
+  }
+
+  findAll() {
+    return `This action returns all card`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} card`;
+  }
+
+  update(id: number, updateCardDto: UpdateCardDto) {
+    return `This action updates a #${id} card`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} card`;
+  }
+}
