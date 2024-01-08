79d842e7fe5fc4f616427fa3aebd5ad7052e64a7
zmjjkk98 <ysyt3199@gmail.com>
2024-01-08 10:29:47 +0900

    chore: 카드틀 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { configModuleValidationSchema } from './config/env-validation.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmModuleOptions } from './config/database.config';
+import { CardModule } from './card/card.module';
 
 
 
@@ -13,6 +14,7 @@ import { typeOrmModuleOptions } from './config/database.config';
       validationSchema: configModuleValidationSchema,
     }),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    CardModule,
   ],
   controllers: [],
   providers: [],
