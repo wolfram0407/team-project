f667edba4de42f88ce99676e6cfd027690bd57d2
Junsik <chzhgod@gmail.com>
2024-01-08 17:39:45 +0900

    Add: Workspace 가드 추가
    1) Req 객체에서 userId와 param 에서 workspaceId 확인 후 workspaceService 의 findOneWorkspace 함수로 존재 여부 확인
    2) Req 객체에 해당 워크스페이스 정보를 담음

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,24 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/req.board';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserInfo } from 'src/common/decorator/user.decorator';
 import { User } from 'src/user/entities/user.entity';
+import { WorkspaceGuard } from 'src/auth/workspace.guard';
+import { WorkspaceInfo } from 'src/common/decorator/workspace.decorator';
+import { Workspace } from 'src/workspaces/entities/workspace.entity';
 
 @ApiTags('Board')
 @Controller('board')
-export class BoardController
-{
-  constructor(private readonly boardService: BoardService) { }
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
 
   @ApiBearerAuth()
   @Post()
-  create(
-    @Body() { title, image_path }: CreateBoardDto,
-    @UserInfo() { userId }: User,
-  )
-  {
-    // 보드 생기는거 확인하고 멤버 추가 하자 
+  create(@Body() { title, image_path }: CreateBoardDto, @UserInfo() { userId }: User) {
+    // 보드 생기는거 확인하고 멤버 추가 하자
     // 유저 멤버 테이블 생성 필요(무조건 생기도록 하자)
     // 생성한 유저가 디폴트로 가입
     return this.boardService.create(userId, title, image_path);
@@ -26,20 +24,13 @@ export class BoardController
 
   @ApiBearerAuth()
   @Get()
-  findAll(@UserInfo() { userId }: User,)
-  {
+  findAll(@UserInfo() { userId }: User) {
     return this.boardService.findAll(userId);
   }
 
-
   @ApiBearerAuth()
   @Get(':id')
-  findOne(
-    @Param('id') id: string,
-    @UserInfo() { userId }: User,
-  )
-  {
+  findOne(@Param('id') id: string, @UserInfo() { userId }: User) {
     return this.boardService.findOne(+id, userId);
   }
-
 }
