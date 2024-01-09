a1ed90233a615b8a7504f3580eab69a315cc169a
minjae Kim <cmg981548@gmail.com>
2024-01-09 10:50:42 +0900

    fix: activity entity(관계 설정)

--- src/board_members/entities/board_members.entity.ts
+++ src/board_members/entities/board_members.entity.ts
@@ -1,44 +1,51 @@
-import { Board } from "src/board/entities/board.entity"
-import { CardMember } from "src/card/entities/card-member.entity"
-import { BoardMemberRole } from "src/common/types/boardMember.type"
-import { User } from "src/user/entities/user.entity"
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Activity } from 'src/activity/entities/activity.entity';
+import { Board } from 'src/board/entities/board.entity';
+import { BoardGrade } from 'src/common/types/boardGrade.type';
+import { BoardMemberRole } from 'src/common/types/boardMember.type';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({
-  name: "b_members",
+  name: 'b_members',
 })
-
-export class BoardMember
-{
+export class BoardMember {
   @PrimaryGeneratedColumn()
-  boardMemberId: number
-
+  boardMemberId: number;
 
   @Column({ type: 'enum', enum: BoardMemberRole, default: BoardMemberRole.Member })
-  role: BoardMemberRole
-
+  role: BoardMemberRole;
 
   @CreateDateColumn()
-  created_at: Date
+  created_at: Date;
   @UpdateDateColumn()
-  updated_at: Date
+  updated_at: Date;
   @DeleteDateColumn()
-  deleted_at: Date
-
+  deleted_at: Date;
 
   @ManyToOne(() => User, (user) => user.boardMember)
   @JoinColumn({ name: 'user_id' })
   user: User;
   @Column()
-  user_id: number
+  user_id: number;
 
   @ManyToOne(() => Board, (board) => board.boardMember)
   @JoinColumn({ name: 'board_id' })
-  boards: Board
+  boards: Board;
   @Column()
-  board_id: number
+  board_id: number;
 
-  @OneToOne(()=> CardMember, (cardMember)=> cardMember.boardMember)
-  cardMember: CardMember
-
+  @OneToMany((type) => Activity, (activity) => activity.boardMember)
+  activity: Activity[];
 }
