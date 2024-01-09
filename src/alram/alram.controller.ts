dff9de22dd0a166f96448b6f31146ec815f6d143
minjae Kim <cmg981548@gmail.com>
2024-01-09 20:59:37 +0900

    fix: alram 스웨거 추가

--- src/alram/alram.controller.ts
+++ src/alram/alram.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Param, Sse } from '@nestjs/common';
 import { AlramService } from './alram.service';
-
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+@ApiBearerAuth()
+@ApiTags('Alram')
 @Controller('alram')
 export class AlramController {
   constructor(private readonly alramService: AlramService) {}
